369f8c6abc9901e07916067acdad21fada989de9
Sezer5 <76732537+Sezer5@users.noreply.github.com>
2023-10-09 23:21:06 +0300

    Root Comments Changed For Example

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+//Sample Comment
 Route::get('/', function () {
     return view('welcome');
 });
