216e8ff9a1aaf32019a2f00715bdedbfe04b3687
Sezer5 <76732537+Sezer5@users.noreply.github.com>
2023-10-13 23:49:41 +0300

    Completed until route prefix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminPanel\CategoryController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminPanel\HomeController as AdminHomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +20,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[HomeController::class,'index'])->name('home');
 Route::get('/test',[HomeController::class,'test'])->name('test');
 
+//ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES
+//ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES
+//ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES
+//ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES ADMİN PANEL ROUTES
 
+Route::get('/admin',[AdminHomeController::class,'index'])->name('index');
+Route::get('/admin/category',[CategoryController::class,'index'])->name('index');
+Route::get('/admin/category/create',[CategoryController::class,'create'])->name('create');
+Route::post('/admin/category/store',[CategoryController::class,'store'])->name('store');
+Route::post('/admin/category/update/{id}',[CategoryController::class,'update'])->name('update');
+Route::get('/admin/category/delete/{id}',[CategoryController::class,'destroy'])->name('destroy');
+Route::get('/admin/category/edit/{id}',[CategoryController::class,'edit'])->name('edit');
 
 Route::middleware([
     'auth:sanctum',
